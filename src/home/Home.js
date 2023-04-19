95c589283f6f89326c54387e9c4dd39ab44a12e6
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-04-19 17:19:05 +0530

    Fetch Dta By Json file

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,18 +1,43 @@
-import React from 'react'
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
-import Productcard from '../productcard/Productcard'
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import Productcard from '../productcard/Productcard';
 
 export default function Home() {
+    // Creating a state for products data.
+    // whenever there is exchange in products , it will force components refresh.
+    const [products, setProducts] = useState([]);
+    // let data = "My Data";
+
+    useEffect(() => {
+        fetch("Product.json")
+            // calling json function
+            .then((res) => res.json())
+            // Listening for json to return.
+            .then((res) => {
+                setProducts(res);
+                console.log(products.length);
+            });
+
+    }, []);
+
     return (
 
         <div>
             <Header />
             <div>
-                <Productcard />
+                { /* <h3>{products && products.length}</h3>
+                <h1>{data}</h1> */ }
+
+                <div className='row'>
+                    {products.map((product) => (
+                        <div className='col-3'>
+                            <Productcard item={product} />
+                        </div>
+                    ))}
+                </div>
             </div>
             <Footer />
-
         </div>
 
     )
--- src/productcard/Productcard.js
+++ src/productcard/Productcard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import "./Productcard.css";
 
-export default function Productcard() {
+export default function Productcard(values) {
     return (
 
         <div className="card" >
-            <img src="https://img1.gadgetsnow.com/gd/images/products/additional/large/G390766_View_1/mobiles/smartphones/apple-iphone-14-128-gb-blue-6-gb-ram-.jpg" className="card-img-top" alt="iphone 13 pro" />
+            <img src={values.item.image} className="card-img-top" alt="img" />
             <div className="card-body"><hr />
-                <h5 className="card-title">iphone 14</h5>
-                <p className="card-text">Your photo. Your font. Your iPhone.</p>
-                <p>Mail. Smart search corrections.</p >
-                <p>Created By Apple.</p>
-                <a href="g" className="btn btn-warning">Buy Now</a>&nbsp;&nbsp;
-                <a href="g" className="btn btn-warning">&#8377;&nbsp;90999</a>
+                <h5 className="card-title">{values.item.name}</h5>
+                <p className="card-text">&#8377;&nbsp;{values.item.price}</p>
+                <p className="card-text">{values.item.Details}</p>
+
+                <a href="img" className="btn btn-warning">Buy Now</a>&nbsp;&nbsp;
+                <a href="img" className="btn btn-success ">Add to cart +</a>
 
             </div>
         </div>
