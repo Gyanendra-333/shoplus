ba996151b2311eb7e99cef0f1b5e80c0b5f13feb
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-04-21 08:55:45 +0530

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>This is Footer Page !</div>
+
+        <div className="card text-center">
+            <div className="card-header">
+                About Us
+            </div>
+            <div className="card-body">
+                <h5 className="card-title">Shoplus India Pvt Ltd </h5><hr/>
+                <p>	&#xA9; Copyright , All rights reserved</p>
+                <p className="card-text">Shoplus Internet Private Limited,  Buildings Alyssa, Begonia & Tech & Fun Village Noida, 560153,  Uttar Pradesh, India</p><hr />
+                <p>Australia, Brazil, Canada, China, France, Germany, Italy, Japan, Poland, Singapore
+                </p>
+                <a href="#" className="btn btn-info">Back to top</a>
+            </div>
+            <div className="card-footer text-body-secondary">
+                2 days ago
+            </div>
+        </div>
+
     )
 }
